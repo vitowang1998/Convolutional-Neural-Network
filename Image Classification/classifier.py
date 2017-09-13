f78755d8c64d9281f4162fc9aef0bbff238a0660
Haoran Wang <vitowang1998@gmail.com>
2017-09-12 23:22:46 -0400

    Initial commit

---
+++ Image Classification/classifier.py
@@ -0,0 +1,20 @@
+import numpy
+
+
+def claasifier(image):
+    # L1 Distance
+    return class_label
+
+
+def train(self, X, y):
+    # In this case we are using a nearest-neighber
+    self.Xtr = X
+    self.Ytr = y
+
+
+
+def predict(model, test_images):
+    return test_labels
+
+
+
